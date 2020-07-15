81a6c8f3c72c23c965e890a07b4968f60937f55b
Wilco de Boer <deboer.wilco@gmail.com>
2020-07-15 03:11:10 +0200

    Add simple proxy code

---
+++ server.py
@@ -0,0 +1,16 @@
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+import cloudscraper
+
+requests = cloudscraper.create_scraper()
+
+
+class CloudProxy(BaseHTTPRequestHandler):
+    def do_GET(self):
+        resp = requests.get(self.path[1:])
+        self.send_response(resp.status_code)
+        self.end_headers()
+        self.wfile.write(resp.content)
+
+
+if __name__ == '__main__':
+    ThreadingHTTPServer(('', 1234), CloudProxy).serve_forever()
