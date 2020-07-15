06efbd3452ed4333f4e6b58bd6064c28b15d8842
Wilco de Boer <deboer.wilco@gmail.com>
2020-07-15 23:58:06 +0200

    Forward CT header

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ class CloudProxy(BaseHTTPRequestHandler):
     def do_GET(self):
         resp = requests.get(self.path[1:])
         self.send_response(resp.status_code)
+        if 'Content-Type' in resp.headers:
+            self.send_header('Content-Type', resp.headers['Content-Type'])
         self.end_headers()
         self.wfile.write(resp.content)
 
