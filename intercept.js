ccfd9f99c8d6335a5161300f159fc88ec8df69c2
Wilco de Boer <deboer.wilco@gmail.com>
2020-07-23 20:57:14 +0200

    Fix more JS requests

--- intercept.js
+++ intercept.js
@@ -1,5 +1,6 @@
 (function (open) {
     XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
+        url = new URL(url, window.location.href.split('/browser/').slice(1).join('/browser/')).href;
         open.call(this, method, '/browser/' + url, async, user, password);
     };
 })(XMLHttpRequest.prototype.open);
